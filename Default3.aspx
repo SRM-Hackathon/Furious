81a09849d740b1948d7cdd08dc3f7df1bb025cde
shivvanigupta <shiwanigg997@gmail.com>
2018-10-12 23:21:59 +0530

    fifth commit

---
+++ Default3.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Buy.master" AutoEventWireup="true" CodeFile="Default3.aspx.cs" Inherits="Default3" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+</asp:Content>
+
---
+++ Default3.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Process process = new Process();
+        process.StartInfo = new ProcessStartInfo
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "cmd.exe",
+            Arguments = "mkdir yukta"
+        };
+        process.Start();
+
+    }
+}
